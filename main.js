57de818008f08d57d419d818e421942c198c8c7b
André Mateus <95716954+Andre-Mateus-Naveca@users.noreply.github.com>
2022-10-06 15:42:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+status1 = "";
+objects = [];
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+}
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(380, 380)
+    canvas.center();
+    video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
+}
+
+function modelLoaded() {
+    console.log("Modelo Carregado!")
+    status1 = true;
+    objectDetector.detect(video, gotResults)
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+
+function draw() {
+
+  image(video, 0, 0, 380, 380);
+  
+    if (status1 != "") {
+        r = random(255)
+        g = random(255)
+        b = random(255)
+        objectDetector.detect(video, gotResults)
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objetos Detectados";
+
+            document.getElementById("numberOfObjects").innerHTML = "Quantidade de objetos detectados: " + objects.length;
+            fill(r,g,b)
+
+            percent = floor(objects[i].confidence * 100)
+
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+
+            noFill()
+
+            stroke(r,g,b)
+
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+    }
+}
